a3677a52b82602de69896929b2b58130083bac69
Marc Feliu (@onepopcorn) <marc.feliu@gmail.com>
2015-03-02 00:40:45 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp   = require('gulp'),
+	jshint = require('gulp-jshint'),
+	uglify = require('gulp-uglifyjs'),
+	html   = require('gulp-htmlhint'),
+	sass   = require('gulp-sass'),
+	clean  = require('gulp-clean'),
+	sync   = require('browser-sync'),
+	plumber= require('gulp-plumber'),
+	colors = require('colors');
+
+var paths = {
+	scripts : 'src/js/**/*.js',
+	styles  : 'src/css/**/*.scss',
+	html    : 'src/*.html',
+	images  : ['src/img/**/*.png','src/img/**/*.jpg','src/img/**/*.gif']
+}
+
+///////// Error handling /////////
+function errorHanlder(err){
+	console.error('['+'error:'.red+']',err.message);
+	console.error('['+'line:'.red+']',err.lineNumber);
+	console.log('['+'file:'.red+']', err.fileName);
+};
+
+
+///////// Cleaning tasks /////////
+gulp.task('markup-clean',function(){
+	return gulp.src('bin/*.html')
+		   .pipe(clean());
+});
+
+gulp.task('scripts-clean',function(){
+	return gulp.src('bin/js/**/*.js')
+		   .pipe(clean());
+});
+
+gulp.task('styles-clean',function(){
+	return gulp.src('bin/css/**/*.css')
+		   .pipe(clean());
+});
+
+///////// Main tasks /////////
+gulp.task('markup',['markup-clean'],function(){
+	return gulp.src(paths.html)
+		   .pipe(plumber())
+		   .pipe(html())
+		   .on('error', errorHanlder)
+		   .pipe(gulp.dest('bin'));
+});
+
+gulp.task('scripts',['scripts-clean'],function(){
+	return gulp.src(paths.scripts)
+		   .pipe(plumber())
+		   .pipe(jshint())
+		   .pipe(jshint.reporter('default'))
+		   .on('error', errorHanlder)
+		   .pipe(uglify({outSourceMap:true}))
+		   .on('error', errorHanlder)
+		   .pipe(gulp.dest('bin/js'));
+});
+
+gulp.task('styles',['styles-clean'],function(){
+	return gulp.src(paths.styles)
+		   .pipe(plumber())
+		   .pipe(sass())
+		   .on('error', errorHanlder)
+		   .pipe(gulp.dest('bin/css'));
+});
+
+///////// Watch tasks /////////
+gulp.task('watch',function(){
+	gulp.watch(paths.scripts,['scripts',sync.reload]);
+	gulp.watch(paths.styles,['styles',sync.reload]);
+	gulp.watch(paths.html,['markup',sync.reload]);
+});
+
+///////// Server /////////
+gulp.task('server',function(){
+	sync({
+		server: {
+			baseDir:'bin'
+		}
+	})
+});
+
+
+gulp.task('default',['markup','scripts','styles','watch','server']);
---
+++ src/js/main.js
@@ -0,0 +1,61 @@
+(function(){
+	var board = document.querySelector('#gameboard'),
+		ROWS = 10,
+		COLS = 10,
+		TOTAL_TILES = ROWS * COLS;
+
+	var clickHandler = function(e){
+		swapClass(e.currentTarget,"wall");
+	};
+
+	var contextHandler = function(e){
+		e.preventDefault();
+		for(var i=0;i<TOTAL_TILES;i++){
+			removeClass(board.children[i],"start");
+		}
+
+		swapClass(e.currentTarget,"start");
+		return;
+	};
+
+	// Initialize board
+	var init = function(){
+		for(var i=0;i<TOTAL_TILES;i++)
+		{
+			var el = document.createElement('div');
+			el.id = i;
+			el.className = 'tile';
+
+			el.addEventListener("click",clickHandler);
+			el.addEventListener("contextmenu",contextHandler);
+			board.appendChild(el);
+		}
+	};
+	// Switch wall class for selected tile
+	var swapClass = function(el,classname){
+		var classes = el.className.split(" ");
+		
+		if(classes.indexOf(classname) === -1)
+		{
+		 	classes.push(classname);
+		} else {
+			var i = classes.indexOf(classname);
+			classes.splice(i,1);
+		}
+
+		el.className = classes.join(" ");
+	};
+
+	var removeClass = function(el,classname){
+		var classes = el.className.split(" "),
+			i = classes.indexOf(classname);
+		if(i !== -1)
+		{
+			classes.splice(i,1);
+			console.log(classes);
+			el.className = classes.join(" ");
+		}
+	}
+	// Entry point
+	init();
+})();
